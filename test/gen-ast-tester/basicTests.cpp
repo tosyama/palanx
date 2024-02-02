786306e09f21ce56bf330148a2010c6fb44d65eb
tosyama <tos-yama@sa2.so-net.ne.jp>
2024-02-02 15:18:26 +0000

    Fix github action.

--- test/gen-ast-tester/basicTests.cpp
+++ test/gen-ast-tester/basicTests.cpp
@@ -2,6 +2,9 @@
 #include "../test-base/testBase.h"
 
 TEST(gen_ast, basic_tests) {
+
+//	string output = execTestCommand("bin/gen-ast ../test/testdata/gen-ast/001_basicPattern.pa");
+
 	string output = execTestCommand("bin/gen-ast ../test/testdata/000_temp.pa");
 
 	ASSERT_EQ(output, "OK");
