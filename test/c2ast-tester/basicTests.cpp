a25b7e7b2b645e8e0b18abb8e3144b79ba59f060
YAMAGUCHI Toshinobu <tos-yama@sa2.so-net.ne.jp>
2024-09-05 12:43:05 +0900

    update c2ast

---
+++ test/c2ast-tester/basicTests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include "../test-base/testBase.h"
+#include "../../lib/json/single_include/nlohmann/json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+TEST(c2ast, basic_tests) {
+    cleanTestEnv();
+    string output;
+    output = execTestCommand("bin/palan-c2ast -s stdio.h");
+    ASSERT_EQ(output, "");
+}
